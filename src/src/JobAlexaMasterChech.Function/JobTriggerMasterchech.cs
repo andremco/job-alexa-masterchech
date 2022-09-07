9cdeff7b7b9d9461315d7947ea8c89aa2137dd65
Andre <andremco1992@gmail.com>
2022-09-07 01:21:29 -0300

    initial project

---
+++ src/src/JobAlexaMasterChech.Function/JobTriggerMasterchech.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace JobAlexaMasterChech.Function
+{
+    public static class JobTriggerMasterchech
+    {
+        [FunctionName("JobTriggerMasterchech")]
+        public static void Run([TimerTrigger("*/10 * * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            if (myTimer.IsPastDue)
+            {
+                log.LogInformation("Timer is running late!");
+            }
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
